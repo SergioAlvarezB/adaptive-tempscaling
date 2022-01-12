c6142f5296153793521a51d4d2fa9270810c1630
Sergio Alvarez Balanya <sergioalvarezba@gmail.com>
2022-01-12 16:49:22 +0100

    change pretrained to compute results for subsampled data

--- code/benchmark_pretrained.py
+++ code/benchmark_pretrained.py
@@ -2,6 +2,7 @@ import os
 import time
 
 import pandas as pd
+import numpy as np
 from utils import softmax
 
 from models import AdaTS, PTS, NanValues
@@ -18,7 +19,7 @@ res_path = '../results/pretrained/'
 
 check_path(res_path)
 
-for i in range(20):
+for i in range(50):
     datasets = [
         'cifar10',
         'cifar100',
@@ -52,210 +53,217 @@ for i in range(20):
         'HTS_torch',
         'HnLTS_torch']
 
-    res_nll = pd.DataFrame(columns=['Dataset', 'Model', 'Uncalibrated'] + TSmodels)
-    res_ECE = pd.DataFrame(columns=['Dataset', 'Model', 'Uncalibrated'] + TSmodels)
-    res_ECE15 = pd.DataFrame(columns=['Dataset', 'Model', 'Uncalibrated'] + TSmodels)
-    res_bri = pd.DataFrame(columns=['Dataset', 'Model', 'Uncalibrated'] + TSmodels)
+    Ns = [100, 1000]
+
+    res_nll = pd.DataFrame(columns=['N', 'Dataset', 'Model', 'Uncalibrated'] + TSmodels)
+    res_ECE = pd.DataFrame(columns=['N', 'Dataset', 'Model', 'Uncalibrated'] + TSmodels)
+    res_ECE15 = pd.DataFrame(columns=['N', 'Dataset', 'Model', 'Uncalibrated'] + TSmodels)
+    res_bri = pd.DataFrame(columns=['N', 'Dataset', 'Model', 'Uncalibrated'] + TSmodels)
 
     t0 = time.time()
     ix = 0
     for model in models:
         for _epochs, lr, dataset in zip(epochs, lrs, datasets):
-            try:
-                train, validation, test = load_precomputedlogits(dataset=dataset,
-                                                                model=model,
-                                                                data_path='../data')
-            except Exception as e:
-                print(e)
-                print('Configuration {}_{} not found.\n'.format(model, dataset))
-                continue
-
-            print('Start benchmarking for configuration {}_{} at time {:.1f}s\n'.format(model, dataset, time.time()-t0))
-
-            X_train, Y_train = train
-            X_val, Y_val = validation
-            X_test, Y_test = test
-
-            N, dim = X_train.shape
-
-            ### Temp-Scal as baseline:
-            tempScaler = TS(dim)
-            tempScaler.fit(X_val, Y_val)
-
-            TSmodels_predictive = {'TS': tempScaler.predictive}
-
-            #### Mix-n-Match Baselines
-            TSmodels_predictive['ETS'] = lambda x: ets_calibrate(X_val, onehot_encode(Y_val), x, dim)
-            TSmodels_predictive['MIR'] = lambda x: mir_calibrate(X_val, onehot_encode(Y_val), x)
-
-            #### BTS baseline
-            print('\tFitting BTS...')
-            bts = BTS()
-            bts.fit(X_val, Y_val, v=True)
-            TSmodels_predictive['BTS'] = bts.predictive
-
-            ##### PTS baseline
-            print('\n\tFitting PTS...')
-            failed = True
-            curr_epochs = _epochs
-            curr_lr = lr
-            while failed:
+            for N in Ns:
                 try:
-                    pts = AdaTS(PTS(dim))
-                    pts = fitAdaTS(pts, X_val, Y_val,
-                                   target_file='../results/pretrained/PTS_{}_{}_{:d}.png'.format(model, dataset, i),
-                                   epochs=curr_epochs, 
-                                   batch_size=1000,
-                                   lr=curr_lr,
-                                   v=True,
-                                   optimizer='sgd')
-                    failed = False
+                    train, validation, test = load_precomputedlogits(dataset=dataset,
+                                                                    model=model,
+                                                                    data_path='../data')
                 except Exception as e:
-                    if type(e) == NanValues:
-                        curr_epochs *= 2
-                        curr_lr /=2
+                    print(e)
+                    print('Configuration {}_{} not found.\n'.format(model, dataset))
+                    continue
 
-            TSmodels_predictive['PTS'] = pts.predictive
+                print('Start benchmarking for configuration {}_{} at time {:.1f}s\n'.format(model, dataset, time.time()-t0))
 
+                X_train, Y_train = train
+                X_val, Y_val = validation
+                X_test, Y_test = test
 
-            ##### PTS-ECE baseline
-            print('\n\tFitting PTS_ece...')
-            failed = True
-            curr_epochs = _epochs
-            curr_lr = lr/10
-            while failed:
-                try:
-                    pts_ece = AdaTS(PTS(dim))
-                    pts_ece = fitAdaTS(pts_ece, X_val, Y_val,
-                                       target_file='../results/pretrained/PTSece_{}_{}_{:d}.png'.format(model, dataset, i),
-                                       loss='ece',
-                                       epochs=curr_epochs,
-                                       batch_size=10000,
-                                       lr=curr_lr,
-                                       v=True,
-                                       optimizer='sgd')
-                    failed = False
-                except Exception as e:
-                    if type(e) == NanValues:
-                        curr_epochs *= 2
-                        curr_lr /=2
+                ### Data Subsampling
+                ix_rand = np.random.permutation(X_val.shape[0])[:N]
+                X_val, Y_val = X_val[ix_rand], Y_val[ix_rand]
 
-            TSmodels_predictive['PTS_ece'] = pts_ece.predictive
+                _, dim = X_train.shape
 
-            #### Our Models
-            print('\n\tFitting LTS...')
-            lts = LTS(dim)
-            lts.fit(X_val, Y_val, v=True)
-            TSmodels_predictive['LTS'] = lts.predictive
+                ### Temp-Scal as baseline:
+                tempScaler = TS(dim)
+                tempScaler.fit(X_val, Y_val)
 
-            print('\n\tFitting LTS_torch...')
-            failed = True
-            curr_epochs = _epochs
-            curr_lr = lr
-            while failed:
-                try:
-                    lts_t = AdaTS(LTS_torch(dim))
-                    lts_t = fitAdaTS(lts_t, X_val, Y_val,
-                                     target_file='../results/pretrained/LTS_{}_{}_{:d}.png'.format(model, dataset, i),
-                                     epochs=curr_epochs,
-                                     batch_size=1000,
-                                     lr=curr_lr,
-                                     v=True,
-                                     optimizer='sgd')
-                    failed = False
-                except Exception as e:
-                    if type(e) == NanValues:
-                        curr_epochs *= 2
-                        curr_lr /=2
-            TSmodels_predictive['LTS_torch'] = lts_t.predictive
+                TSmodels_predictive = {'TS': tempScaler.predictive}
+
+                #### Mix-n-Match Baselines
+                TSmodels_predictive['ETS'] = lambda x: ets_calibrate(X_val, onehot_encode(Y_val), x, dim)
+                TSmodels_predictive['MIR'] = lambda x: mir_calibrate(X_val, onehot_encode(Y_val), x)
+
+                #### BTS baseline
+                print('\tFitting BTS...')
+                bts = BTS()
+                bts.fit(X_val, Y_val, v=True)
+                TSmodels_predictive['BTS'] = bts.predictive
+
+                ##### PTS baseline
+                print('\n\tFitting PTS...')
+                failed = True
+                curr_epochs = _epochs
+                curr_lr = lr
+                while failed:
+                    try:
+                        pts = AdaTS(PTS(dim))
+                        pts = fitAdaTS(pts, X_val, Y_val,
+                                    target_file='../results/pretrained/PTS_{}_{}_{:d}.png'.format(model, dataset, i),
+                                    epochs=curr_epochs, 
+                                    batch_size=1000,
+                                    lr=curr_lr,
+                                    v=True,
+                                    optimizer='sgd')
+                        failed = False
+                    except Exception as e:
+                        if type(e) == NanValues:
+                            curr_epochs *= 2
+                            curr_lr /=2
+
+                TSmodels_predictive['PTS'] = pts.predictive
 
 
-            print('\n\tFitting HTS...')
-            hts = HTS(dim)
-            hts.fit(X_val, Y_val, v=True)
-            TSmodels_predictive['HTS'] = hts.predictive
+                ##### PTS-ECE baseline
+                print('\n\tFitting PTS_ece...')
+                failed = True
+                curr_epochs = _epochs
+                curr_lr = lr/10
+                while failed:
+                    try:
+                        pts_ece = AdaTS(PTS(dim))
+                        pts_ece = fitAdaTS(pts_ece, X_val, Y_val,
+                                        target_file='../results/pretrained/PTSece_{}_{}_{:d}.png'.format(model, dataset, i),
+                                        loss='ece',
+                                        epochs=curr_epochs,
+                                        batch_size=10000,
+                                        lr=curr_lr,
+                                        v=True,
+                                        optimizer='sgd')
+                        failed = False
+                    except Exception as e:
+                        if type(e) == NanValues:
+                            curr_epochs *= 2
+                            curr_lr /=2
 
-            print('\n\tFitting HTS_torch...')
-            failed = True
-            curr_epochs = _epochs
-            curr_lr = lr
-            while failed:
-                try:
-                    hts_t = AdaTS(HTS_torch(dim))
-                    hts_t = fitAdaTS(hts_t, X_val, Y_val,
-                                     target_file='../results/pretrained/HTS_{}_{}_{:d}.png'.format(model, dataset, i),
-                                     epochs=curr_epochs,
-                                     batch_size=1000,
-                                     lr=curr_lr,
-                                     v=True,
-                                     optimizer='sgd')
-                    failed = False
-                except Exception as e:
-                    if type(e) == NanValues:
-                        curr_epochs *= 2
-                        curr_lr /=2
-            TSmodels_predictive['HTS_torch'] = hts_t.predictive
+                TSmodels_predictive['PTS_ece'] = pts_ece.predictive
 
+                #### Our Models
+                print('\n\tFitting LTS...')
+                lts = LTS(dim)
+                lts.fit(X_val, Y_val, v=True)
+                TSmodels_predictive['LTS'] = lts.predictive
+
+                print('\n\tFitting LTS_torch...')
+                failed = True
+                curr_epochs = _epochs
+                curr_lr = lr
+                while failed:
+                    try:
+                        lts_t = AdaTS(LTS_torch(dim))
+                        lts_t = fitAdaTS(lts_t, X_val, Y_val,
+                                        target_file='../results/pretrained/LTS_{}_{}_{:d}.png'.format(model, dataset, i),
+                                        epochs=curr_epochs,
+                                        batch_size=1000,
+                                        lr=curr_lr,
+                                        v=True,
+                                        optimizer='sgd')
+                        failed = False
+                    except Exception as e:
+                        if type(e) == NanValues:
+                            curr_epochs *= 2
+                            curr_lr /=2
+                TSmodels_predictive['LTS_torch'] = lts_t.predictive
+
+
+                print('\n\tFitting HTS...')
+                hts = HTS(dim)
+                hts.fit(X_val, Y_val, v=True)
+                TSmodels_predictive['HTS'] = hts.predictive
+
+                print('\n\tFitting HTS_torch...')
+                failed = True
+                curr_epochs = _epochs
+                curr_lr = lr
+                while failed:
+                    try:
+                        hts_t = AdaTS(HTS_torch(dim))
+                        hts_t = fitAdaTS(hts_t, X_val, Y_val,
+                                        target_file='../results/pretrained/HTS_{}_{}_{:d}.png'.format(model, dataset, i),
+                                        epochs=curr_epochs,
+                                        batch_size=1000,
+                                        lr=curr_lr,
+                                        v=True,
+                                        optimizer='sgd')
+                        failed = False
+                    except Exception as e:
+                        if type(e) == NanValues:
+                            curr_epochs *= 2
+                            curr_lr /=2
+                TSmodels_predictive['HTS_torch'] = hts_t.predictive
+
+                
+                
+
+                print('\n\tFitting HnLTS...')
+                hnlts = HnLTS(dim)
+                hnlts.fit(X_val, Y_val, v=True)
+                TSmodels_predictive['HnLTS'] = hnlts.predictive
+
+                print('\n\tFitting HnLTS_torch...')
+                failed = True
+                curr_epochs = _epochs
+                curr_lr = lr
+                while failed:
+                    try:
+                        hnlts_t = AdaTS(HnLTS_torch(dim))
+                        hnlts_t = fitAdaTS(hnlts_t, X_val, Y_val,
+                                        target_file='../results/pretrained/HnLTS_{}_{}_{:d}.png'.format(model, dataset, i),
+                                        epochs=curr_epochs,
+                                        batch_size=1000,
+                                        lr=curr_lr,
+                                        v=True,
+                                        optimizer='sgd')
+                        failed = False
+                    except Exception as e:
+                        if type(e) == NanValues:
+                            curr_epochs *= 2
+                            curr_lr /=2
+                TSmodels_predictive['HnLTS_torch'] = hnlts_t.predictive
+
+
+
+                acc, ece, bri, nll, mce = compute_metrics(X_test, Y_test, M=50, from_logits=True)
+                ece_15 = (compute_ece(softmax(X_test, axis=-1), Y_test))
+
+                eces = []
+                bris = []
+                nlls = []
+                eces_15 = []
+
+                for label, TSmodel in TSmodels_predictive.items():
+                    TSmetrics = compute_metrics(TSmodel(X_test),
+                                                Y_test,
+                                                M=50,
+                                                from_logits=False)
+
+                    eces_15.append(compute_ece(TSmodel(X_test), Y_test))
+                    eces.append(TSmetrics[1])
+                    bris.append(TSmetrics[2])
+                    nlls.append(TSmetrics[3])
+
+
+                res_ECE15.loc[ix] = [N, dataset, model, ece_15] + eces_15
+                res_ECE.loc[ix] = [N, dataset, model, ece] + eces
+                res_nll.loc[ix] = [N, dataset, model, nll] + nlls
+                res_bri.loc[ix] = [N, dataset, model, bri] + bris
+
+                ix += 1
             
-            
 
-            print('\n\tFitting HnLTS...')
-            hnlts = HnLTS(dim)
-            hnlts.fit(X_val, Y_val, v=True)
-            TSmodels_predictive['HnLTS'] = hnlts.predictive
-
-            print('\n\tFitting HnLTS_torch...')
-            failed = True
-            curr_epochs = _epochs
-            curr_lr = lr
-            while failed:
-                try:
-                    hnlts_t = AdaTS(HnLTS_torch(dim))
-                    hnlts_t = fitAdaTS(hnlts_t, X_val, Y_val,
-                                       target_file='../results/pretrained/HnLTS_{}_{}_{:d}.png'.format(model, dataset, i),
-                                       epochs=curr_epochs,
-                                       batch_size=1000,
-                                       lr=curr_lr,
-                                       v=True,
-                                       optimizer='sgd')
-                    failed = False
-                except Exception as e:
-                    if type(e) == NanValues:
-                        curr_epochs *= 2
-                        curr_lr /=2
-            TSmodels_predictive['HnLTS_torch'] = hnlts_t.predictive
-
-
-
-            acc, ece, bri, nll, mce = compute_metrics(X_test, Y_test, M=50, from_logits=True)
-            ece_15 = (compute_ece(softmax(X_test, axis=-1), Y_test))
-
-            eces = []
-            bris = []
-            nlls = []
-            eces_15 = []
-
-            for label, TSmodel in TSmodels_predictive.items():
-                TSmetrics = compute_metrics(TSmodel(X_test),
-                                            Y_test,
-                                            M=50,
-                                            from_logits=False)
-
-                eces_15.append(compute_ece(TSmodel(X_test), Y_test))
-                eces.append(TSmetrics[1])
-                bris.append(TSmetrics[2])
-                nlls.append(TSmetrics[3])
-
-
-            res_ECE15.loc[ix] = [dataset, model, ece_15] + eces_15
-            res_ECE.loc[ix] = [dataset, model, ece] + eces
-            res_nll.loc[ix] = [dataset, model, nll] + nlls
-            res_bri.loc[ix] = [dataset, model, bri] + bris
-
-            ix += 1
-            
-
-    res_ECE15.to_csv('../results/pretrained/logitsJuan_ECE15_reg_{:d}.csv'.format(i))
-    res_ECE.to_csv('../results/pretrained/logitsJuan_ECE_reg_{:d}.csv'.format(i))
-    res_nll.to_csv('../results/pretrained/logitsJuan_NLL_reg_{:d}.csv'.format(i))
-    res_bri.to_csv('../results/pretrained/logitsJuan_BRI_reg_{:d}.csv'.format(i))
+    res_ECE15.to_csv('../results/pretrained/logitsJuan_ECE15_red_{:d}.csv'.format(i))
+    res_ECE.to_csv('../results/pretrained/logitsJuan_ECE_red_{:d}.csv'.format(i))
+    res_nll.to_csv('../results/pretrained/logitsJuan_NLL_red_{:d}.csv'.format(i))
+    res_bri.to_csv('../results/pretrained/logitsJuan_BRI_red_{:d}.csv'.format(i))
