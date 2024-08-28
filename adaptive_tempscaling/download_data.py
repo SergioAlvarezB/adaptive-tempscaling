75d17966199718c53da38e27f86f5a4c26f6bd9a
Sergio Alvarez <sergioalvarezbalanya1995@gmail.com>
2024-08-28 11:44:00 +0200

    refactor

---
+++ adaptive_tempscaling/download_data.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import requests
+import shutil
+import tarfile
+
+import numpy as np
+
+from torch.utils import data
+from torchvision import transforms
+from torchvision.datasets import CIFAR10, SVHN
+import torchvision.transforms as transforms
+
+from utils import check_path
+
+
+CIFAR10_PATH = '../data/CIFAR10'
+CIFAR10C_PATH = '../data'
+SVHN_PATH = '../data/SVHN'
+
+CIFAR10C_URL = 'https://zenodo.org/record/2535967/files/CIFAR-10-C.tar'
+
+check_path(CIFAR10_PATH)
+check_path(CIFAR10C_PATH)
+check_path(SVHN_PATH)
+
+##############################################
+############ Download CIFAR10-C ##############
+##############################################
+target_path = os.path.join(CIFAR10C_PATH, 'CIFAR-10-C.tar')
+print('Downloading CIFAR10-C...')
+with open(target_path, 'wb') as f:
+    with requests.get(CIFAR10C_URL, allow_redirects=True, stream=True) as resp:
+        total_length = resp.headers.get('content-length')
+        dl = 0
+        total_length = int(total_length)
+        for chunk in resp.iter_content(chunk_size=4096):
+            if chunk:
+                dl += len(chunk)
+                f.write(chunk)
+                done = int(50 * dl / total_length)
+                sys.stdout.write("\r[{:s}{:s}{:s}] {:.2f}%".format('=' * done, '>', ' ' * (50-done), (dl/total_length)*100) )    
+                sys.stdout.flush()
+
+# Extract
+print('Extracting CIFAR10-C...')
+tarf = tarfile.open(target_path, 'r')
+tarf.extractall(CIFAR10C_PATH)
+tarf.close()
+os.remove(target_path)
+print('Done')
+
+##############################################
+############ Download CIFAR10 ################
+##############################################
+print('Downloading CIFAR10...')
+
+transforms_data=transforms.Compose([
+    transforms.ToTensor(),
+])
+
+train = CIFAR10(CIFAR10_PATH, train=True, download=True, transform=transforms_data)
+test = CIFAR10(CIFAR10_PATH, train=False, transform=transforms_data)
+
+# Divide into validation and train splits
+N = len(train)
+
+np.random.seed(10)
+rand_ix = np.random.permutation(N)
+
+ix_train, ix_val = rand_ix[:45000], rand_ix[45000:]
+
+train_data = data.Subset(train, ix_train)
+val_data = data.Subset(train, ix_val)
+
+train = data.DataLoader(train_data, batch_size=45000)
+val = data.DataLoader(val_data, batch_size=5000)
+test = data.DataLoader(test, batch_size=10000)
+
+X_train, y_train = next(iter(train))
+X_val, y_val = next(iter(val))
+X_test, y_test = next(iter(test))
+
+X_train, y_train = (255*X_train.numpy()).astype(np.uint8), y_train.numpy()
+X_val, y_val = (255*X_val.numpy()).astype(np.uint8), y_val.numpy()
+X_test, y_test = (255*X_test.numpy()).astype(np.uint8), y_test.numpy()
+
+X_train = np.transpose(X_train, (0, 2,3,1))
+X_val= np.transpose(X_val, (0, 2,3,1))
+X_test = np.transpose(X_test, (0, 2,3,1))
+
+for arrs, name in zip([(X_train, y_train), (X_val, y_val), (X_test, y_test)],
+                      ['train', 'val', 'test']):
+    np.save(os.path.join(CIFAR10_PATH, name + '_imas.npy'), arrs[0])
+    np.save(os.path.join(CIFAR10_PATH, name + '_labels.npy'), arrs[1])
+
+
+os.remove(os.path.join(CIFAR10_PATH, 'cifar-10-python.tar.gz'))
+shutil.rmtree(os.path.join(CIFAR10_PATH, 'cifar-10-batches-py'))
+print('Done')
+
+##############################################
+############## Download SVHN #################
+##############################################
+
+print('Downloading SVHN...')
+
+svhn = SVHN(SVHN_PATH, split='test', download=True, transform=transforms_data)
+svhn = data.DataLoader(svhn, batch_size=len(svhn))
+
+X, y = next(iter(svhn))
+X, y = (255*X.numpy()).astype(np.uint8), y.numpy()
+X = np.transpose(X, (0, 2,3,1))
+
+np.save(os.path.join(SVHN_PATH, 'imas.npy'), X)
+np.save(os.path.join(SVHN_PATH, 'labels.npy'), y)
+
+os.remove(os.path.join(SVHN_PATH, 'test_32x32.mat'))
+print('Done')
---
+++ adaptive_tempscaling/train_model.py
@@ -0,0 +1,246 @@
+from ctypes import ArgumentError
+import os
+import time
+import argparse
+
+import numpy as np
+import torch
+
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import MultiStepLR
+from torchvision.models import resnet50, resnet101, densenet121
+import torchvision.transforms as transforms
+from adaptive_tempscaling.models import LeNet5
+
+from adaptive_tempscaling.utils import NumpyDataset, check_path, predict_logits
+
+
+CIFAR10_PATH = '../data/CIFAR10'
+SAVE_PATH = '../trained_models/CIFAR10/'
+MODELS = ['densenet121', 'resnet101', 'resnet50', 'lenet5']
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--model', help='model to train', type=str.lower,
+                        required=True)
+    parser.add_argument('--dev', help='Device to use', type=str.lower, default='cpu')
+
+    parser.add_argument('--batch_size', help='Mini-batch size to use for training',
+                        type=int, default=128)
+    parser.add_argument('--epochs', help='Number of epochs',
+                        type=int, default=200)
+    parser.add_argument('--lr', nargs='+', help='Learning rates to try',
+                        default=[1e-4, 1e-3], type=float)
+    parser.add_argument('-wd', '--weight_decay', nargs='+', help='Values of weight decay to use',
+                        default=np.geomspace(1e-4, 1e-2, 3), type=float)
+    parser.add_argument('--pretrained', help='If set loads pretrained model', action='store_true')
+
+    return parser.parse_args()
+
+
+
+def train_model(init_model,
+                train_dataloader,
+                val_dataloader,
+                lrs=[1e-4, 1e-3],
+                weight_decays=np.geomspace(1e-4, 1e-2, 3),
+                epochs=200,
+                dev=torch.device('cpu')):
+
+    best_val = np.inf
+
+    for lr in lrs:
+        for weight_decay in weight_decays:
+            print('Start new configuration:')
+            print('\t Learning rate: {:.2e}'.format(lr))
+            print('\t Weight Decay: {:.2e}'.format(weight_decay))
+
+            loss = []
+            acc = []
+
+            net = init_model()
+            optimizer = torch.optim.SGD(net.parameters(),
+                                        lr=lr, momentum=0.9,
+                                        nesterov=True,
+                                        weight_decay=weight_decay)
+            CE = torch.nn.functional.cross_entropy
+
+            lr_sched = MultiStepLR(optimizer, milestones=[100, 150])
+
+            t = time.time()
+
+            net.to(dev)
+
+            for e in range(epochs):
+                loss.append(0)
+                acc.append(0)
+                net.train()
+                N = 0
+                for x, y in train_dataloader:
+                    x, y = x.to(dev), y.to(dev)
+
+                    n = x.shape[0]
+                    N += n
+
+                    preds = net(x)
+                    _loss = CE(preds, y, reduction='mean')
+                    loss[-1] += n*_loss.item()
+                    _, _preds = torch.max(preds, dim=1)
+                    acc[-1] += torch.sum((_preds == y).float()).item() * 100.
+                    
+                    optimizer.zero_grad()
+                    _loss.backward()
+                    optimizer.step()
+                    
+                
+                acc[-1] /= N # Size of training set
+                loss[-1] /= N
+            
+                if e <= int(0.8*epochs):
+                    lr_sched.step()
+                
+                if e%5==1:  
+                    print('End of epoch: {:d}'.format(e))
+                    print("Time: {:.3f}s; train loss: {:.3f}".format(time.time() - t, loss[-1]))
+                    print("\t train accuracy: {:.2f}%".format(acc[-1]))
+
+            net.eval()
+            val_loss = 0
+            val_acc = 0
+            Nval = 0
+            for x, y in val_dataloader:
+                x = x.to(dev)
+
+                Nval += x.shape[0]
+
+                preds = net(x).detach().cpu()
+                _loss = CE(preds, y, reduction='sum')
+                val_loss += _loss.item()
+                _, _preds = torch.max(preds, dim=1)
+                val_acc += torch.sum((_preds == y).float()).item() * 100.
+            
+            val_acc /= Nval # Size of validation set
+            val_loss /= Nval
+            print('End of epoch: {:d}'.format(e))
+            print("Time: {:.3f}s; train loss: {:.3f}, validation loss: {:.3f}".format(time.time() - t, loss[-1], val_loss))
+            print("\t train accuracy: {:.2f}%, validation accuracy: {:.2f}%".format(acc[-1], val_acc))
+
+            if val_loss < best_val:
+                print('Found best configuration:')
+                print('\t Learning rate: {:.2e}'.format(lr))
+                print('\t Weight Decay: {:.2e}'.format(weight_decay))
+                best_model = net
+                best_val = val_loss
+
+    return best_model
+
+
+
+
+def main():
+
+    conf = parse_arguments()
+    dev = torch.device(conf.dev) if torch.cuda.is_available() \
+        else torch.device('cpu')
+
+    if conf.model not in MODELS:
+        raise ArgumentError('model not recognized, must be one of {}\n'.format(MODELS))
+
+    check_path(os.path.join(SAVE_PATH, conf.model))
+
+    if conf.model == 'densenet121':
+        def init_model():
+            net = densenet121()
+            net.classifier = torch.nn.Linear(1024, 10)
+            return net
+
+    if conf.model == 'resnet101':
+        def init_model():
+            net = resnet101()
+            net.fc = torch.nn.Linear(2048 , 10)
+            return net
+
+    if conf.model == 'resnet50':
+        def init_model():
+            net = resnet50()
+            net.fc = torch.nn.Linear(2048 , 10)
+            return net
+
+    elif conf.model == 'lenet5':
+        def init_model():
+            net = LeNet5(dim=10, input_channels=3)
+            return net
+
+
+    cifar10_transforms_train=transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ]) #transforms are different for train and test
+    cifar10_transforms_test=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    ## Load Data
+    X_train = np.load(os.path.join(CIFAR10_PATH, 'train_imas.npy'))
+    y_train = np.load(os.path.join(CIFAR10_PATH, 'train_labels.npy'))
+
+    X_val = np.load(os.path.join(CIFAR10_PATH, 'val_imas.npy'))
+    y_val = np.load(os.path.join(CIFAR10_PATH, 'val_labels.npy'))
+
+    X_test = np.load(os.path.join(CIFAR10_PATH, 'test_imas.npy'))
+    y_test = np.load(os.path.join(CIFAR10_PATH, 'test_labels.npy'))
+
+    N = X_train.shape[0]
+    Nval = X_val.shape[0]
+
+
+    train_data = NumpyDataset(X_train, y_train, transform=cifar10_transforms_train)
+    val_data = NumpyDataset(X_val, y_val, transform=cifar10_transforms_test)
+
+    train_dataloader = DataLoader(train_data, batch_size=conf.batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_data, batch_size=conf.batch_size)
+
+    if not conf.pretrained:
+        net = train_model(init_model,
+                        train_dataloader,
+                        val_dataloader,
+                        lrs=conf.lr,
+                        epochs=conf.epochs,
+                        weight_decays=conf.weight_decay,
+                        dev=dev)
+        torch.save(net, os.path.join(SAVE_PATH, conf.model+'.pth'))
+    else:
+        try:
+            net = torch.load(os.path.join(SAVE_PATH, conf.model+'.pth'))
+        except Exception as e:
+            raise e
+
+
+    # Precompute output-logits
+    test_data = NumpyDataset(X_test, y_test, transform=cifar10_transforms_test)
+    test_dataloader = DataLoader(test_data, batch_size=conf.batch_size)
+
+    train_data_test = NumpyDataset(X_train, y_train, transform=cifar10_transforms_test)
+    train_dataloader_test = DataLoader(train_data_test, batch_size=conf.batch_size)
+    train_logits = predict_logits(net, train_dataloader_test, dev=dev)
+    val_logits = predict_logits(net, val_dataloader, dev=dev)
+    test_logits = predict_logits(net, test_dataloader, dev=dev)
+
+    print('Accuracy on the different sets:')
+    print('\t Train set: {:.2f}'.format(100.0 * np.mean(np.argmax(train_logits, axis=1) == y_train)))
+    print('\t Val set: {:.2f}'.format(100.0 * np.mean(np.argmax(val_logits, axis=1) == y_val)))
+    print('\t Test set: {:.2f}'.format(100.0 * np.mean(np.argmax(test_logits, axis=1) == y_test)))
+
+
+    np.save(os.path.join(SAVE_PATH, conf.model, 'train_logits'), train_logits)
+    np.save(os.path.join(SAVE_PATH, conf.model, 'val_logits'), val_logits)
+    np.save(os.path.join(SAVE_PATH, conf.model, 'test_logits'), test_logits)
+
+
+if __name__ == '__main__':
+    main()
